9557d56626933a4dcdeb834193fea732ac25dc87
unknown <stanj89@gmail.com>
2019-05-08 21:37:41 -0400

    done.

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,15 +15,15 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
-'use strict'
+"use strict";
 function GameObject(attributes) {
   this.createdAt = Date();
   this.name = attributes.name;
-  this.dimensions= attributes.dimensions;
+  this.dimensions = attributes.dimensions;
 }
 GameObject.prototype.destroy = function() {
-  return(`${this.name} was removed from the game.`);
-}
+  return `${this.name} was removed from the game.`;
+};
 
 /*
   === CharacterStats ===
@@ -38,8 +38,8 @@ function CharacterStats(stats) {
 CharacterStats.prototype = Object.create(GameObject.prototype);
 
 CharacterStats.prototype.takeDamage = function takeDamage() {
-  return(`${this.name} took damage.`);
-}
+  return `${this.name} took damage.`;
+};
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
   * team
@@ -59,8 +59,8 @@ function Humanoid(humanAttributes) {
 Humanoid.prototype = Object.create(CharacterStats.prototype);
 
 Humanoid.prototype.greet = function greet() {
-  return (`${this.name} offers a greeting in ${this.language}`);
-}
+  return `${this.name} offers a greeting in ${this.language}`;
+};
 /*
  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -69,68 +69,58 @@ Humanoid.prototype.greet = function greet() {
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1
+  },
+  healthPoints: 5,
+  name: "Bruce",
+  team: "Mage Guild",
+  weapons: ["Staff of Shamalama"],
+  language: "Common Tongue"
+});
 
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Tongue',
-  });
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2
+  },
+  healthPoints: 15,
+  name: "Sir Mustachio",
+  team: "The Round Table",
+  weapons: ["Giant Sword", "Shield"],
+  language: "Common Tongue"
+});
 
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Tongue',
-  });
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4
+  },
+  healthPoints: 10,
+  name: "Lilith",
+  team: "Forest Kingdom",
+  weapons: ["Bow", "Dagger"],
+  language: "Elvish"
+});
 
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
-
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
 // Stretch task:
 // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
@@ -144,17 +134,15 @@ Warlock.prototype = Object.create(Humanoid.prototype);
 
 Warlock.prototype.eldritchBlast = function(target) {
   let spellTarget = target;
-  let caster = this.name
-  let damage = this.spellPower*10
-  return {
-    chaosDamage: function() {
-      Object.defineProperty(spellTarget, "healthPoints", {value : spellTarget.healthPoints-damage});
-      if (spellTarget.healthPoints > 0){
-      return (`${caster} summons a blast of pure chaos descending from the Eldritch Moon itself. ${spellTarget.name} takes ${damage} chaos damage. ${spellTarget.name} has ${spellTarget.healthPoints} HP remaining.`)
-      } else return ((`${caster}'s chaotic ways have torn ${spellTarget.name} to pieces, ${spellTarget.name} has died`))
+  let caster = this.name;
+  let damage = this.spellPower * 10;
+  return function() {
+      Object.defineProperty(spellTarget, "healthPoints", {value: spellTarget.healthPoints - damage});
+      if (spellTarget.healthPoints > 0) {
+        return `${caster} summons a blast of pure chaos descending from the Eldritch Moon itself. ${spellTarget.name} takes ${damage} chaos damage. ${spellTarget.name} has ${spellTarget.healthPoints} HP remaining.`;
+      } else return `${caster}'s chaotic ways have torn ${spellTarget.name} to pieces, ${spellTarget.name} has died.`;
     }()
-}
-}
+  };
 
 function Shadowdancer(goodAttributes) {
   Humanoid.call(this, goodAttributes);
@@ -164,17 +152,15 @@ Shadowdancer.prototype = Object.create(Humanoid.prototype);
 
 Shadowdancer.prototype.psychicKnife = function(target) {
   let spellTarget = target;
-  let caster = this.name
-  let damage = this.spellPower*10
-  return {
-    psychicDamage: function() {
-      Object.defineProperty(spellTarget, "healthPoints", {value : spellTarget.healthPoints-damage});
-      if (spellTarget.healthPoints > 0){
-      return (`OMG ${caster} stabs ${spellTarget.name} with a flurry of stabs from his Psychic Knife! ${spellTarget.name} takes ${damage} psychic damage. ${spellTarget.name} has ${spellTarget.healthPoints} HP remaining.`)
-      } else return (`${caster}'s Psychic power overwhelms ${spellTarget.name}, ${spellTarget.name} has died`)
-  }()
-}
-}
+  let caster = this.name;
+  let damage = this.spellPower * 10;
+  return function() {
+      Object.defineProperty(spellTarget, "healthPoints", {value: spellTarget.healthPoints - damage});
+      if (spellTarget.healthPoints > 0) {
+        return `OMG ${caster} stabs ${spellTarget.name} with a flurry of stabs from his Psychic Knife! ${spellTarget.name} takes ${damage} psychic damage. ${spellTarget.name} has ${spellTarget.healthPoints} HP remaining.`;
+      } else return `${caster}'s Psychic power overwhelms ${spellTarget.name}, ${spellTarget.name} has died.`;
+    }()
+  };
 
 function GymnastWizard(ambiguousAttributes) {
   Humanoid.call(this, ambiguousAttributes);
@@ -185,33 +171,28 @@ GymnastWizard.prototype = Object.create(Humanoid.prototype);
 GymnastWizard.prototype.backflipAppleToss = function(target1, target2) {
   let spellTarget1 = target1;
   let spellTarget2 = target2;
-  let caster = this
-  let damage = this.spellPower*10
-  return {
-    sacrificialFruitHeal: function() {
-      Object.defineProperty(spellTarget1, "healthPoints", {value : spellTarget1.healthPoints+damage});
-      Object.defineProperty(spellTarget2, "healthPoints", {value : spellTarget2.healthPoints+damage});
-      Object.defineProperty(caster, "healthPoints", {value : caster.healthPoints-(2*damage)});
-      return (`${caster.name} appears athletically from the shadows and casts her signature spell Backflip Apple Toss. The sacrificial fruit heal successfully increases ${spellTarget1.name}'s HP to ${spellTarget1.healthPoints} and ${spellTarget2.name}'s HP to ${spellTarget2.healthPoints}, but lowers ${caster.name}'s own HP to ${caster.healthPoints}. No springs attached!`)
-  }()
-}
-}
+  let caster = this;
+  let damage = this.spellPower * 10;
+  return function() {
+      Object.defineProperty(spellTarget1, "healthPoints", {value: spellTarget1.healthPoints + damage});
+      Object.defineProperty(spellTarget2, "healthPoints", {value: spellTarget2.healthPoints + damage});
+      Object.defineProperty(caster, "healthPoints", {value: caster.healthPoints - 2 * damage});
+      return `${caster.name} appears athletically from the shadows and casts her signature spell Backflip Apple Toss. The sacrificial fruit heal successfully increases ${spellTarget1.name}'s HP to ${spellTarget1.healthPoints} and ${spellTarget2.name}'s HP to ${spellTarget2.healthPoints}, but lowers ${caster.name}'s own HP to ${caster.healthPoints}. No springs attached!`;
+    }()
+  };
 
 const badDude = new Warlock({
   createdAt: new Date(),
   dimensions: {
     length: 4,
     width: 1,
-    height: 1,
+    height: 1
   },
   healthPoints: 100,
-  name: 'Schmebulock',
-  team: 'Gnome',
-  weapons: [
-    'Cantrips',
-    'Wand',
-  ],
-  language: 'Gnomish',
+  name: "Schmebulock",
+  team: "Gnome",
+  weapons: ["Cantrips", "Wand"],
+  language: "Gnomish",
   spellPower: 7
 });
 
@@ -220,34 +201,28 @@ const goodDude = new Shadowdancer({
   dimensions: {
     length: 10,
     width: 3,
-    height: 4,
+    height: 4
   },
   healthPoints: 100,
-  name: 'Hendrix',
-  team: 'Myconid',
-  weapons: [
-    'Shadows',
-    'Breakdancing',
-  ],
-  language: 'Myconish?',
+  name: "Hendrix",
+  team: "Myconid",
+  weapons: ["Shadows", "Breakdancing"],
+  language: "Myconish?",
   spellPower: 5
 });
 
 const mumbleBunny = new GymnastWizard({
   createdAt: new Date(),
   dimensions: {
-    length: .1,
-    width: .1,
-    height: .1,
+    length: 0.1,
+    width: 0.1,
+    height: 0.1
   },
   healthPoints: 100,
-  name: 'Mumble Bunny',
-  team: 'Gymnasts',
-  weapons: [
-    'Springs',
-    'Somersaults',
-  ],
-  language: 'Pommel Horse',
+  name: "Mumble Bunny",
+  team: "Gymnasts",
+  weapons: ["Springs", "Somersaults"],
+  language: "Pommel Horse",
   spellPower: 2
 });
 console.log(goodDude);
@@ -256,8 +231,20 @@ console.log(mumbleBunny);
 console.log(goodDude.psychicKnife(badDude));
 console.log(badDude.eldritchBlast(goodDude));
 console.log(goodDude.psychicKnife(badDude));
-console.log(goodDude.psychicKnife(mumbleBunny));
 console.log(mumbleBunny.backflipAppleToss(badDude, goodDude));
+console.log(goodDude.psychicKnife(mumbleBunny));
 console.log(badDude.eldritchBlast(goodDude));
 
-//I spent way too much time on this...
+/*I spent way too much time on this...
+
+OMG Hendrix stabs Schmebulock with a flurry of stabs from his Psychic Knife! Schmebulock takes 50 psychic damage. Schmebulock has 50 HP remaining.
+
+Schmebulock summons a blast of pure chaos descending from the Eldritch Moon itself. Hendrix takes 70 chaos damage. Hendrix has 30 HP remaining.
+
+Hendrix's Psychic power overwhelms Schmebulock, Schmebulock has died.
+
+Mumble Bunny appears athletically from the shadows and casts her signature spell Backflip Apple Toss. The sacrificial fruit heal successfully increases Schmebulock's HP to 20 and Hendrix's HP to 50, but lowers Mumble Bunny's own HP to 10. No springs attached!
+
+OMG Hendrix stabs Mumble Bunny with a flurry of stabs from his Psychic Knife! Mumble Bunny takes 50 psychic damage. Mumble Bunny has 50 HP remaining.
+
+Schmebulock's chaotic ways have torn Hendrix to pieces, Hendrix has died.*/
